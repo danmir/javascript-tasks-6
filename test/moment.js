3186c6c366f506d85c408fac6b264e78ab9b0e64
danmir <danmir@me.com>
2015-11-09 21:33:09 +0500

    Не понимаю, что с тестами, локально все проходит

--- test/moment.js
+++ test/moment.js
@@ -6,10 +6,14 @@ var robbery = require('../robbery');
 describe('Базовое тестирование объекта moment', function () {
     it('format()', function () {
         var gang = require('fs').readFileSync('gang.json', 'utf-8');
-        var robberyMoment = robbery.getAppropriateMoment(gang, 90, {
-            from: '09:00+5',
-            to: '21:00+5'
-        });
+        var robberyMoment = robbery.getAppropriateMoment(
+            gang,
+            90,
+            {
+                from: '09:00+5',
+                to: '21:00+5'
+            }
+        );
 
         var actual = robberyMoment.format(
             'Ограбление должно состоятся в %DD. Всем быть готовыми к %HH:%MM!'
